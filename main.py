b214172a8d48728fe67a73c9c72900ead976a19f
Robin <robin.prevotat@efrei.net>
2023-05-26 08:37:46 +0200

    display-count

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ def menu():
     print("\n==== Programming Quotes ====")
     print("1. Random quote")
     print("2. All quotes")
-    print("3. Exit")
+    print("3. Several quotes")
+    print("4. Exit")
 
 def main():
     while True:
@@ -17,7 +18,11 @@ def main():
             print_quote(random_quote(quotes))
         elif choice == "2":
             view_quotes(quotes)
+
         elif choice == "3":
+            count = int(input("Enter the number of quotes to display: "))
+            display_quotes(quotes, count)
+        elif choice == "4":
             print("Good bye...")
             break
         else:
