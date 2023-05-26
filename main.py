3975f19552512dcb8c4e91cd0d75d7146e950c37
Tiphanie Dépréaux <tiphaniedepreaux@gmail.com>
2023-05-26 08:58:43 +0200

    Input changes

--- main.py
+++ main.py
@@ -2,34 +2,31 @@ from functions import *
 
 def menu():
     print("\n==== Programming Quotes ====")
-    print("1. Random quote")
-    print("2. All quotes")
-    print("3. Several quotes")
-    print("4. Add quote")
-    print("5. Exit")
+    print("random : Random quote")
+    print("display : Display quotes")
+    print("add : Add a new quote")
+    print("all : Display all quotes")
+    print("exit : Exit the program")
 
 def main():
     while True:
         quotes = load_quotes("quotes.txt")
         menu()
 
-        choice = input("Choose your an action (1-4): ")
+        choice = input(">> ")
         
-        if choice == "1":
+        if choice == "random":
             print_quote(random_quote(quotes))
-        elif choice == "2":
-            view_quotes(quotes)
-
-        elif choice == "3":
+        elif choice == "display":
             count = int(input("Enter the number of quotes to display: "))
             display_quotes(quotes, count)
-            
-        elif choice == "4":
-            add_quote(quotes,"quotes.txt")
-        elif choice == "5":
+        elif choice == "add":
+            add_quote(quotes, "quotes.txt")
+        elif choice == "all":
+            view_quotes(quotes)
+        elif choice == "exit":
             print("Good bye...")
             break
-
         else:
             print("Invalid input")
 
