d6a94b7fd941dc2beffe9b034a0916e6f8070501
Robin <robin.prevotat@efrei.net>
2023-05-26 08:15:07 +0200

    display_count add

--- functions.py
+++ functions.py
@@ -20,3 +20,11 @@ def print_quote(quote):
 def view_quotes(quotes):
     for quote in quotes:
         print_quote(quote)
+def display_quotes(quotes, count):
+    if count >= len(quotes):
+        print("All Quotes:")
+        view_quotes(quotes)
+    else:
+        print(f"First {count} Quotes:")
+        for i in range(count):
+            print_quote(quotes[i])
